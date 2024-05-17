aba9567147f258495d6e0b5b0a3555559ed64c6e
Arrivera <andresramirorivera@gmail.com>
2024-05-16 20:48:45 -0700

    Fix setup backend

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import express from 'express'
-import { PORT } from './config.js'
-import userRoutes from './routes/users.routes.js'
-import morgan from 'morgan'
+import express from "express";
+import usersRoutes from "./routes/users.routes.js";
+import morgan from "morgan";
+import { PORT } from "./config.js";
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
+
+// Middlewares
 app.use(express.json());
-app.use(userRoutes);
+app.use(express.urlencoded({ extended: false }));
+
+app.use(usersRoutes);
 
 app.listen(PORT);
-console.log('Server on port', PORT);
+console.log("Server on port", PORT);
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,21 +1,19 @@
-import { Router } from 'express'
-import { getUser, getUsers, createUser, updateUser, deleteUser } from '../controllers/users.controllers.js';
+import { Router } from "express";
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/users.controllers.js";
 
 const router = Router();
 
-// Get all users
-router.get('/users', getUsers);
-
-//Get one user
-router.get('/users/:id', getUser);
-
-//Post a user
-router.post('/users', createUser);
-
-//Delete a user
-router.delete('/users/:id', deleteUser);
-
-//Edit a user
-router.put('/users/:id', updateUser);
+//Rest api
+router.get("/users", getUsers);
+router.get("/users/:id", getUserById);
+router.post("/users", createUser);
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
 export default router;
